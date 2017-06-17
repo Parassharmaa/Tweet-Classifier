b8d709a4ba35b687cf57b07a5870cb4fb6530850
Paras Sharma <paraazz@outlook.com>
2017-06-17 16:54:02 +0530

    Update example.py

--- example.py
+++ example.py
@@ -6,7 +6,7 @@ T = Tweetifier("paraazz", 100)
 
 #crawl or fetch the tweets max: 3200
 T.crawl()
-crawled_tweets = cur.tweets
+crawled_tweets = T.tweets
 
 #classify the tweets topic wise 
 #(Multinomial Naive Bayes Classifier accuracy: 78%)
